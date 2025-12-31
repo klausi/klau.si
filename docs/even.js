8ef473c94d141db6fd0af4157629dcd853defe00
Klaus Purer <klaus.purer@protonmail.ch>
2025-12-31 16:48:17 +0100

    refactor(menu): Replace mobile menu JS with a CSS version

--- docs/even.js
+++ docs/even.js
@@ -1,35 +1,3 @@
-function initMobile() {
-  var $mobileNav = document.getElementById("mobile-navbar");
-  var $mobileNavIcon = document.querySelector(".mobile-navbar-icon");
-
-  var slideout = new Slideout({
-    "panel": document.getElementById("mobile-panel"),
-    "menu": document.getElementById("mobile-menu"),
-    "padding": 180,
-    "tolerance": 70
-  });
-  slideout.disableTouch();
-
-  $mobileNavIcon.addEventListener("click", function () {
-    slideout.toggle();
-  });
-
-  slideout.on("beforeopen", function () {
-    $mobileNav.classList.add("fixed-open");
-    $mobileNavIcon.classList.add("icon-click");
-    $mobileNavIcon.classList.remove("icon-out");
-  });
-
-  slideout.on("beforeclose", function () {
-    $mobileNav.classList.remove("fixed-open");
-    $mobileNavIcon.classList.add("icon-out");
-    $mobileNavIcon.classList.remove("icon-click");
-  });
-
-  document.getElementById("mobile-panel").addEventListener("touchend", function () {
-    slideout.isOpen() && $mobileNavIcon.click();
-  })
-}
 function initToc() {
   var $toclink = document.querySelectorAll('.toc-link')
   var $headerlink = document.querySelectorAll('.post-content h1 , .post-content h2')
@@ -98,11 +66,9 @@ function initYoutubeLazy() {
 if (document.readyState === "complete" ||
   (document.readyState !== "loading" && !document.documentElement.doScroll)
 ) {
-  initMobile();
   initToc();
   initYoutubeLazy();
 } else {
-  document.addEventListener("DOMContentLoaded", initMobile);
   document.addEventListener("DOMContentLoaded", initToc);
   document.addEventListener("DOMContentLoaded", initYoutubeLazy);
 }
